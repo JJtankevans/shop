a645faae5fd532bb3373c5a16a79e90ae64e573e
JJtankevans <vitorflamengao2@gmail.com>
2025-03-28 14:46:40 -0400

    feature:define-the-main-screen-and-the-product-item-screen

---
+++ lib/models/product.dart
@@ -0,0 +1,22 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavirote;
+
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFavirote = false
+  });
+
+  void toggleFavorite() {
+    isFavirote = !isFavirote;
+  }
+}
